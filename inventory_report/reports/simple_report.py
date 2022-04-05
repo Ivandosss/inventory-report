02432c89142f0eb4fd1ab2c8dc20f697aeb3dfe6
Pop-Os <ivan@pop-os.localdomain>
2022-04-04 23:24:34 -0300

    Requisito um

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,1 +1,25 @@
-# Requisito 
+from datetime import datetime
+from collections import Counter
+
+
+class SimpleReport:
+    @classmethod
+    def generate(cls, inventory):
+        manufacturing_date = []
+
+        expired_dates = []
+        companies = []
+        for product in inventory:
+            manufacturing_date.append(product['data_de_fabricacao'])
+            dateNow = str(datetime.now().today())
+            if product['data_de_validade'] > dateNow:
+                expired_dates.append(product['data_de_validade'])
+
+            companies.append(product['nome_da_empresa'])
+
+        common = Counter(companies).most_common()[0][0]
+        return (
+            f"Data de fabricação mais antiga: {min(manufacturing_date)}\n"
+            f"Data de validade mais próxima: {min(expired_dates)}\n"
+            f"Empresa com maior quantidade de produtos estocados: {common}\n"
+        )
